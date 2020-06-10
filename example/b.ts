8c38d6cdf436322ac42dd2664174448a97ad31c9
shynome <shynome@shy-f117.lan>
2020-06-11 02:39:26 +0800

    no faster

--- example/b.ts
+++ example/b.ts
@@ -1,4 +1,4 @@
-import { ServerRequest } from "https://deno.land/std@0.56.0/http/server.ts";
+import type { ServerRequest } from "https://deno.land/std@0.56.0/http/server.ts";
 
 export default (req: ServerRequest) => {
   req.respond({ body: "bbbb" });
