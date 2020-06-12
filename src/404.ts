74e0903074116084f0c0864e5443c5cbf709ff96
shynome <shynome@shy-f117.lan>
2020-06-12 13:58:00 +0800

    return 404 status code

--- src/404.ts
+++ src/404.ts
@@ -1,3 +1,7 @@
-export default () => {
-  return "404";
+import { ServerRequest } from "./deps.ts";
+export default (req: ServerRequest) => {
+  req.respond({
+    status: 404,
+    body: "no api rule match",
+  });
 };
