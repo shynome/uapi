a001fbc4b8cdf31f6c95db958831e275b1e9b77e
shynome <shynome@gmail.com>
2023-10-07 09:34:30 +0800

    添加配套测试

--- router_test.ts
+++ router_test.ts
@@ -13,6 +13,18 @@ Deno.test("http test", async () => {
     ["/foo/rest/bar", 200, "...rest"],
     ["/foo/rest2/bar", 200, "...rest"],
     ["/404", 404, null],
+    // [rest]
+    ["/rest", 200, "rest"],
+    ["/rest/foo", 200, "rest"],
+    ["/rest/foo/bar", 404, null],
+    // [...rest]
+    ["/rest-all", 200, "rest all"],
+    ["/rest-all/foo", 200, "rest all"],
+    ["/rest-all/foo/bar", 200, "rest all"],
+    // mix [rest] [...rest]
+    ["/rest-mix", 200, "rest"],
+    ["/rest-mix/foo", 200, "rest"],
+    ["/rest-mix/foo/bar", 200, "rest all"],
   ];
 
   const uapi = new UAPIRouter(path.resolve("./example"));
